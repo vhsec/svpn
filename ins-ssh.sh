45e19d31bbd54723b68a4a5ea57f6b8b7f76d18b
Unix <root@localhost>
2022-09-27 19:36:48 +0700

    up

--- ins-ssh.sh
+++ ins-ssh.sh
@@ -68,5 +68,16 @@ END
 
 # ws.py
 cp ws.py /etc/svpn
+cp badvpn /usr/bin/badvpn-udpgw
+chmod +x /usr/bin/badvpn-udpgw
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7400 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7500 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7600 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7700 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7800 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7900 --max-clients 500
 screen -dm python2 /etc/svpn/ws.py 2082
 service stunnel start
